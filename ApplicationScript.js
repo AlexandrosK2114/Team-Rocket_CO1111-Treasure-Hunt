9679a67e7a71846c64fb31316ac5ff2748fd9130
kostas kosta <teamphoenix.uclan@gmail.com>
2026-02-12 13:40:07 +0200

    Cookie creation and InstaScan API( reads QR)

--- ApplicationScript.js
+++ ApplicationScript.js
@@ -1,3 +1,37 @@
+//a function sets them every time the client enters the site
+function setCookie(cookieName, cookieValue, expireDays) {
+    let date = new Date();
+    date.setTime(date.getTime() + (expireDays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + date.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
+var date = new Date();
+setCookie("date", date.toUTCString());
+var expires = "expires=" + date.toUTCString();
+
+setCookie("firstname", expires);
+
+
+console.log(getCookie("Firstname"));
+console.log(getCookie("Lastname"));
+
+//A Function gets the cookie documents
+function  getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie =
+        decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 async function getChallenges() {
 
     let huntsArray = [];
