a28de31c09d574ed45781a03e16aa72a56691061
Gregory2k6 <241988138+Gregory2k6@users.noreply.github.com>
2026-02-25 12:40:14 +0200

    app.js added buttons for the true false statement

--- JSFiles/AppJS.js
+++ JSFiles/AppJS.js
@@ -8,40 +8,39 @@ function loadQuestion() {
         .then(response => response.json()) // Parse JSON text to JavaScript object
         .then(jsonObject => {
 
+            console.log(jsonObject);
             let status=jsonObject.status;
 
-            if(status==="OK"){
-                if(!jsonObject.completed){
+            if(status==="OK") {
+                if (!jsonObject.completed) {
 
-                    let question=jsonObject.questionText;
+                    let question = jsonObject.questionText;
                     console.log(question);
-                    let type=jsonObject.questionType;
-                    let questionScore=jsonObject.currentScore;
-                    let questionDiv=document.getElementById("questionDivision");
+                    let type = jsonObject.questionType;
+                    let questionScore = jsonObject.currentScore;
+                    let questionDiv = document.getElementById("questionDivision");
 
-                    let questionTexBox=document.getElementById("questionTextBox");
-                    questionTexBox.innerHTML+="<p>"+question+"</p>";
+                    let questionTexBox = document.getElementById("questionTextBox");
+                    questionTexBox.innerHTML += "<p>" + question + "</p>";
 
-                    if(type==="BOOLEAN"){
+                    if (type === "BOOLEAN") {
+                        let answerOptions = document.getElementById("questionAnswers");
+                        let newElement1 = "<button id='trueBtn' class='tf-btn' onClick='selectAnswer(true)'>True</button>";
+                        answerOptions.innerHTML += newElement1;
+                        let newElement2 = "<button id='falseBtn' class='tf-btn' onclick='selectAnswer(false)'>False</button>";
+                        answerOptions.innerHTML += newElement2;
+                    } else if (type === "INTEGER") {
+
+                    } else if (type === "NUMERIC") {
+
+                    } else if (type === "MCQ") {
+
+                    } else if (type === "TEXT") {
 
                     }
-                    else if(type==="INTEGER"){
 
-                    }
-                    else if(type==="NUMERIC"){
-
-                    }
-                    else if(type==="MCQ"){
-
-                    }
-                    else if(type==="TEXT"){
-
-                    }
-
-                }
+                } else if (status === "ERROR")
+                    alert(jsonObject.errorMessages);
             }
-            else if(status==="ERROR")
-                alert(jsonObject.errorMessages);
-
         })
 }
